f76cad50c7a8d3beb9ca3584d47b8c6934e4860e
MONIRUZZAMAN <moniruzzaman@MONIRUZZAMANs-MacBook-Air.local>
2022-12-15 00:36:36 +0600

    create tour route added. tour service added. get all tours api implimented with good response format

--- models/Tour.model.js
+++ models/Tour.model.js
@@ -6,6 +6,34 @@ const tourSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  ratings: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  views: {
+    type: Number,
+    required: true,
+  },
 });
 
 //Models
